55c6e5a300a034a654845e1117cf3a1d64e52d0f
Luis Garrillo <lggarrillo@urbe.edu.ve>
2024-12-22 22:13:00 -0400

    Create tetromino and screen assets

--- main.cpp
+++ main.cpp
@@ -1,7 +1,90 @@
 #include <iostream>
+#include <windows.h>
+
+using namespace std;
+void createAssets();
+int rotate(int px, int py, int rotation);
+
+wstring tetromino[7];
+int fieldWidth = 12;
+int fieldHeight = 18;
+unsigned char* playingField = nullptr;
+
+wchar_t* screen = nullptr;
+int screenWidth = 80;
+int screenHeight = 30;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Create assets
+    createAssets();
+    
+    playingField = new unsigned char[fieldWidth * fieldHeight];
+    for (int x = 0; x < fieldWidth; x++) {
+        for (int y = 0; y < fieldHeight; y++) {
+            playingField[y * fieldWidth + x] = (x == 0 || x == fieldWidth - 1 || y == fieldHeight - 1) ? 9 : 0;
+        }
+    }
+
+    screen = new wchar_t[screenWidth * screenHeight];
+    for (int i = 0; i < screenWidth * screenHeight; i++) {
+        screen[i] = L' ';
+    }
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    // Game Logic
+
+    WriteConsoleOutputCharacter(hConsole, screen, screenWidth * screenHeight, { 0,0 }, &dwBytesWritten);
+    return 0;
+
+}
+
+void createAssets() {
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"....");
+
+    tetromino[2].append(L".X..");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L"....");
+
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"....");
+
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"....");
+
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L"..X.");
+    tetromino[5].append(L"..X.");
+
+    tetromino[6].append(L"....");
+    tetromino[6].append(L".XX.");
+    tetromino[6].append(L".X..");
+    tetromino[6].append(L".X..");
+}
+
+int rotate(int px, int py, int rotation) {
+    switch (rotation % 4) {
+    case 0: return py * 4 + px;
+    case 1: return 12 + py - (px * 4);
+    case 2: return 15 - (py * 4) + px;
+    case 3: return 3 - py + (px * 4);
+    }
+    return 0;
 }
 
